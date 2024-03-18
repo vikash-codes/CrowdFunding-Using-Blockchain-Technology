cb52776402e2cecdba843aff435b930c5eb198ef
Devangana26 <devanganabarua@gmail.com>
2024-03-18 18:48:44 +0530

    Hero completed

--- Components/Hero.jsx
+++ Components/Hero.jsx
@@ -75,6 +75,7 @@ md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
                   Campaign
                 </h3>
                 <form>
+
                   <div className="mb-1 sm:mb-2">
                     <label
                       htmlFor="firstName"
@@ -96,8 +97,92 @@ md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
                       id="firstName"
                       name="firstName"
                     />
-
                   </div>
+
+                  <div className="mb-1 sm:mb-2">
+                    <label
+                      htmlFor="lastName"
+                      className="inline-block mb-1 font-medium"
+                    >
+                      Description
+                    </label>
+                    <input
+                      onChange={(e) =>
+                        setCampaign({
+                          ...campaign,
+                          description: e.target.value,
+                        })
+                      }
+                      placeholder="description"
+                      required
+                      type="text"
+                      className="flex-grow w-full h−12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+                      id="lastName"
+                      name="lastName"
+                    />
+                  </div>
+
+                  <div className="mb-1 sm:mb-2">
+                    <label
+                      htmlFor="email"
+                      className="inline-block mb-1 font-medium"
+                    >
+                      Target Amount
+                    </label>
+                    <input
+                      onChange={(e) =>
+                        setCampaign({
+                          ...campaign,
+                          amount: e.target.value,
+                        })
+                      }
+                      placeholder="amount"
+                      required
+                      type="text"
+                      className="flex-grow w-full h−12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+                      id="email"
+                      name="email"
+                    />
+                  </div>
+
+                  <div className="mb-1 sm:mb-2">
+                    <label
+                      htmlFor="email"
+                      className="inline-block mb-1 font-medium"
+                    >
+                      Deadline
+                    </label>
+                    <input
+                      onChange={(e) =>
+                        setCampaign({
+                          ...campaign,
+                          deadline: e.target.value,
+                        })
+                      }
+                      placeholder="Date"
+                      required
+                      type="date"
+                      className="flex-grow w-full h−12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+                      id="email"
+                      name="email"
+                    />
+                  </div>
+
+                  <div className="mt-4 mb-2 sm:mb-4">
+                    <button
+                      onClick={(e) => createNewCampaign(e)}
+                      type="submit"
+                      className="inline-flex items-center justify-center w-full h-12 px-6 font-semibold tracking-wide text-black transition duration-200 ounded shadow-md bg-deep-blue-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none newColor"
+                      >
+                        Create Campaign
+                      </button>
+                  </div>
+
+                  <p className="text-ts text-gray-600 sm:text-sm">
+                      Create your Campaign for raise funds
+                  </p>
+
+
                 </form>
               </div>
             </div>
